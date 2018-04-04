e839fbdae6944addb820893af9bfe19adad11d1b
Milos Gajdos <milosgajdos83@gmail.com>
2018-04-04 22:12:48 +0100

    Major refactoring. Client device scan separated from handlers. Config.

--- main.go
+++ main.go
@@ -1,38 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/zmb3/spotify"
 )
 
-// redirectURI is the OAuth redirect URI for the application.
-const redirectURI = "http://localhost:8080/callback"
+const (
+	cliname = "alertify"
+)
+
+var (
+	// redirectURI is the OAuth redirect URI for the application.
+	redirectURI string
+	// deviceName is Spotify client device name
+	deviceName string
+	// deviceID is Spotify client device ID
+	deviceID string
+	// songURI is Spotify song URI
+	songURI string
+)
+
+func init() {
+	flag.StringVar(&redirectURI, "redirect-uri", "http://localhost:8080/callback", "OAuth app redirect URI set in Spotify API console")
+	flag.StringVar(&deviceName, "device-name", "", "Spotify device name as recognised by Spotify API")
+	flag.StringVar(&deviceID, "device-id", "", "Spotify device ID as recognised by Spotify API")
+	flag.StringVar(&songURI, "song-uri", "spotify:track:2xYlyywNgefLCRDG8hlxZq", "Spotify song URI")
+	// disable timestamps and set prefix
+	log.SetFlags(0)
+	log.SetPrefix("[ " + cliname + " ] ")
+}
 
 // authHandler handles OAuth2 authentication callback from Spotify API
-func authHandler(auth *spotify.Authenticator, state string, ch chan *spotify.Client) http.Handler {
+func authHandler(auth *Auth, ch chan *spotify.Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tok, err := auth.Token(state, r)
+		tok, err := auth.Token(auth.State, r)
 		if err != nil {
-			http.Error(w, "Couldn't retrieve OAauth2 token", http.StatusForbidden)
+			http.Error(w, "Couldn't retrieve OAuth token", http.StatusForbidden)
 			log.Fatal(err)
 		}
-		if s := r.FormValue("state"); s != state {
+		if s := r.FormValue("state"); s != auth.State {
 			http.NotFound(w, r)
-			log.Fatalf("State mismatch: %s != %s\n", s, state)
+			log.Fatalf("State mismatch: %s != %s\n", s, auth.State)
 		}
 		// use the token to get an authenticated client
 		client := auth.NewClient(tok)
 		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(w, "Login successful!")
+		fmt.Fprintf(w, "Spotify login successful!")
 		ch <- &client
 	})
 }
 
 // configHandler returns configuration encoded in JSON
-func configHandler(client *spotify.Client) http.Handler {
+func configHandler(client *Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// use the client to make calls that require authorization
 		user, err := client.CurrentUser()
@@ -55,71 +79,197 @@ func configHandler(client *spotify.Client) http.Handler {
 }
 
 // alertHandler starts playing configured music
-func alertHandler(client *spotify.Client) http.Handler {
+func alertHandler(client *Client, songURI string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		deviceID := new(spotify.ID)
-		*deviceID = "f6e2bbe128cd0b9b5137ee18dd5afdc34b6a2598"
-		opts := &spotify.PlayOptions{
-			DeviceID: deviceID,
-			URIs:     []spotify.URI{"spotify:track:2xYlyywNgefLCRDG8hlxZq"},
-		}
-		if err := client.PlayOpt(opts); err != nil {
-			log.Printf("Couldnt play song: %v\n", err)
-			http.Error(w, "Couldnt play song", http.StatusServiceUnavailable)
+		if err := client.PlaySong(songURI); err != nil {
+			log.Printf("Error playing the song: %v\n", err)
+			http.Error(w, "Couldnnt play song", http.StatusServiceUnavailable)
 			return
 		}
 	})
 }
 
 // stopHandler stops playing the alert song
-func stopHandler(client *spotify.Client) http.Handler {
+func stopHandler(client *Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		deviceID := new(spotify.ID)
-		*deviceID = "f6e2bbe128cd0b9b5137ee18dd5afdc34b6a2598"
-		opts := &spotify.PlayOptions{
-			DeviceID: deviceID,
-		}
-		if err := client.PauseOpt(opts); err != nil {
-			log.Printf("Couldnt pause song: %v\n", err)
-			http.Error(w, "Couldnt play song", http.StatusServiceUnavailable)
-			return
+		if err := client.Pause(); err != nil {
+			log.Printf("Error stopping the playback: %v\n", err)
+			http.Error(w, "Couldnt play alert song", http.StatusServiceUnavailable)
 		}
 	})
 }
 
-func main() {
+// Config contains alertify configuration
+type Config struct {
+	// RedirectURI is Spotify app OAuth redirect URI
+	RedirectURI string
+	// DeviceName is Spotify device name
+	DeviceName string
+	// DeviceID is Spotify device ID
+	DeviceID string
+	// SongURI is Spotify song URI
+	SongURI string
+}
+
+func parseConfigFlags() (*Config, error) {
+	// parse flags
+	flag.Parse()
+
+	if spotifyID := os.Getenv("SPOTIFY_ID"); spotifyID == "" {
+		return nil, fmt.Errorf("Could not read SPOTIFY_ID environment variable")
+	}
+
+	if spotifyID := os.Getenv("SPOTIFY_SECRET"); spotifyID == "" {
+		return nil, fmt.Errorf("Could not read SPOTIFY_SECRET environment variable")
+	}
+
+	return &Config{
+		RedirectURI: redirectURI,
+		DeviceName:  deviceName,
+		DeviceID:    deviceID,
+		SongURI:     songURI,
+	}, nil
+}
+
+// Auth allows to authenticate with Spotify API
+type Auth struct {
+	// Spotify authenticator
+	*spotify.Authenticator
+	// State is OAuth state
+	State string
+	// RedirectURI is Spotify RedirectURI
+	RedirectURI string
+}
+
+// NewAuth returns Auth to authenticate with Spotify API
+func NewAuth(redirectURI string, state string) *Auth {
+	// Spotify API authenticator
 	auth := spotify.NewAuthenticator(redirectURI,
 		spotify.ScopeUserReadCurrentlyPlaying,
 		spotify.ScopeUserReadPlaybackState,
 		spotify.ScopeUserModifyPlaybackState)
-	state := "abc123"
+
+	return &Auth{&auth, state, redirectURI}
+}
+
+func (a *Auth) URL() string {
+	// Spotify Login URL
+	return a.AuthURL(a.State)
+}
+
+// Client wrapt spotifyClient
+type Client struct {
+	*spotify.Client
+	// DeviceID
+	deviceID *spotify.ID
+}
+
+func (c *Client) DeviceID() *spotify.ID {
+	return c.deviceID
+}
+
+func (c *Client) SetDeviceID(cfg *Config) error {
+	if cfg.DeviceID != "" {
+		deviceID := spotify.ID(cfg.DeviceID)
+		c.deviceID = &deviceID
+		return nil
+	}
+	// get all Spotify devices
+	devices, err := c.PlayerDevices()
+	if err != nil {
+		return err
+	}
+	var activeDevices []spotify.PlayerDevice
+	// loop over available devices
+	for _, device := range devices {
+		if cfg.DeviceName != "" {
+			if device.Name == cfg.DeviceName {
+				c.deviceID = &device.ID
+				return nil
+			}
+		}
+		if !device.Restricted {
+			activeDevices = append(activeDevices, device)
+		}
+	}
+
+	if len(activeDevices) != 0 {
+		c.deviceID = &activeDevices[0].ID
+		return nil
+	}
+
+	return fmt.Errorf("No active device found!")
+}
+
+func (c *Client) PlaySong(songURI string) error {
+	opts := &spotify.PlayOptions{
+		DeviceID: c.DeviceID(),
+		URIs:     []spotify.URI{spotify.URI(songURI)},
+	}
+
+	if err := c.PlayOpt(opts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Pause() error {
+	opts := &spotify.PlayOptions{
+		DeviceID: c.DeviceID(),
+	}
+
+	if err := c.PauseOpt(opts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	// alertify configuration
+	config, err := parseConfigFlags()
+	if err != nil {
+		log.Fatalf("Error parsing cli flags: %v", err)
+	}
+
+	auth := NewAuth(config.RedirectURI, "abc123")
+	fmt.Println("\nLog in to Spotify by visiting the following page in your browser:", auth.URL())
+
+	// Spotify client channel
 	clientChan := make(chan *spotify.Client)
+	// HTTP API error channel
 	errChan := make(chan error, 1)
 
 	// Create HTTP muxer
 	h := http.NewServeMux()
-
-	h.Handle("/callback", authHandler(&auth, state, clientChan))
-
+	// Catch all handler
 	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got request for:", r.URL.String())
+		log.Printf("Got request for: %s", r.URL.String())
 	})
+	// Auth handler: need to go through OAuth flow first
+	h.Handle("/callback", authHandler(auth, clientChan))
 
+	// Start the API service
 	go func() {
+		// TODO: handle server shutdown
 		errChan <- http.ListenAndServe(":8080", h)
 	}()
 
-	url := auth.AuthURL(state)
-	fmt.Println("Log in to Spotify by visiting the following page in your browser:", url)
-
 	// wait for auth to complete
-	client := <-clientChan
+	spotifyClient := <-clientChan
+	// Set device ID
+	deviceID := spotify.ID(config.DeviceID)
+	client := &Client{spotifyClient, &deviceID}
+	if err := client.SetDeviceID(config); err != nil {
+		log.Fatal(err)
+	}
 
-	// register new http handlers
+	// register alertify API handlers
 	h.Handle("/config", configHandler(client))
-	h.Handle("/alert", alertHandler(client))
+	h.Handle("/alert", alertHandler(client, config.SongURI))
 	h.Handle("/stop", stopHandler(client))
 
-	err := <-errChan
+	err = <-errChan
 	log.Fatal(err)
 }
